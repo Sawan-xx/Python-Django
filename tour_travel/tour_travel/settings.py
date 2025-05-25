46823d9e0b280f3e5400686ede982eff424c7d43
sawan dhakse <sawandhakse20@gmail.com>
2025-05-25 19:14:18 +0530

    added url in setting.py

--- tour_travel/tour_travel/settings.py
+++ tour_travel/tour_travel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-aq*99$x+k7tkx6xlh+_+fuy8!*eefl0vz_*g9e_u_^9+l=l*(+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://shubhamtourandtravels.onrender.com']
+ALLOWED_HOSTS = ['https://shubhamtourandtravels.onrender.com/']
 
 
 # Application definition
